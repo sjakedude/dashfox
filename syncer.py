64035b5c3c5672dbcb258b6b2983d0a8cdf52b0f
Jake <sjakedude@gmail.com>
2024-05-12 11:51:44 -0400

    WIP

--- syncer.py
+++ syncer.py
@@ -40,7 +40,7 @@ class Syncer:
             print(new_token)
         year = new_tokens[-1]
         day = new_tokens[-2]
-        month = new_tokens[-3]
+        month = "bla"
         print(f"Last Updated: {month} {day} {year}")
         print("asdf")
 
