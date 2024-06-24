898a3e38aa1b7ae5142349dba198a1a8c78e80cd
sjakedude <sjakedude@gmail.com>
2024-06-24 15:08:42 -0400

    WIP

--- syncer.py
+++ syncer.py
@@ -2,23 +2,8 @@
 # and checks every save file metadata for when it was last modified and syncs the
 # save files to the other consoles so they are all up to date with the latest save
 
-import subprocess
 import json
 from subprocess import check_output
-from subprocess import CalledProcessError
-from datetime import datetime
-
-# Gamertag ID
-XBOX_PROFILE_ID = "E00000E4D88A136A" # sjakedude
-
-# Console IP addresses
-JASPER_RGH_IP = "192.168.0.233"
-OFFICE_RGH_IP = "192.168.0.234"
-
-# Game Title ID and Save File Name
-GAME_TITLE_ID = "425307D5"
-GAME_SAVE_NAME = "Gears2Checkpoint"
-
 
 def read_json():
     with open("xbox_config.json", 'r') as file:
@@ -28,10 +13,10 @@ def read_json():
 
 class Syncer:
 
-    def __init__(self, profile, game_names):
+    def __init__(self, profile, game):
         self.config = read_json()
         self.profile = self.verify_profile(profile)
-        self.game_names = self.verify_game_names(game_names)
+        self.game = self.verify_game(game)
         self.xboxs = self.get_xbox_ips()
         self.metadata = self.create_metadata_dict()
 
@@ -43,22 +28,17 @@ class Syncer:
             print(f"Profile {profile} not supported")
 
 
-    def verify_game_names(self, game_names):
-        games = game_names.split("|")
-        for game in games:
-            try:
-                self.config["xbox_games"][game]
-            except KeyError:
-                print(f"Game {game} not supported")
-        return games
+    def verify_game(self, game):
+        try:
+            self.config["xbox_games"][game]
+        except KeyError:
+            print(f"Game {game} not supported")
         
 
     def create_metadata_dict(self):
         metadata = {}
         for xbox in self.xboxs:
-            metadata[xbox] = {}
-            for game_name in self.game_names:
-                metadata[xbox][game_name] = None
+            metadata[xbox] = {self.game: None}
         return metadata
 
     def get_xbox_ips(self):
@@ -67,62 +47,24 @@ class Syncer:
             xboxs.append(self.config["xbox_ips"][key])
         return xboxs
 
-    def convert_month(self, month):
-        if month == "Jan":
-            return 1
-        elif month == "Feb":
-            return 2
-        elif month == "Mar":
-            return 3
-        elif month == "Apr":
-            return 4
-        elif month == "May":
-            return 5
-        elif month == "Jun":
-            return 6
-        elif month == "Jul":
-            return 7
-        elif month == "Aug":
-            return 8
-        elif month == "Sep":
-            return 9
-        elif month == "Oct":
-            return 10
-        elif month == "Nov":
-            return 11
-        elif month == "Dec":
-            return 12
 
-    def get_last_modified(self, response, save_file_name):
-        print("RESSPONSE")
-        print(response)
-        tokens = response.split("-rwxrwxrwx")
-        print("tokens")
-        print(tokens)
-        metadata_line = tokens[1]
-        return "bla"
-        date = tokens[1].split(save_file_name)[0]
-        metadata_line_tokens = date.split(" ")
-        year = metadata_line_tokens[-2]
-        day = metadata_line_tokens[-3]
-        month = self.convert_month(metadata_line_tokens[-4])
-        return f"{month}/{day}/{year}"
+    def get_last_modified(self, response):
+        epoch_ts = response.split("date -r . +%s \r\n")[1].split("\r\nquit")[0]
+        return epoch_ts
 
     def get_latest_save_file(self):
         xbox_with_latest_save = None
         latest_date = None
         for xbox in self.metadata:
-            date = datetime.strptime(self.metadata[xbox][GAME_SAVE_NAME], "%m/%d/%Y")
+            date = self.metadata[xbox][self.game]
             if not latest_date:
                 latest_date = date
                 xbox_with_latest_save = xbox
             if latest_date < date:
                 xbox_with_latest_save = xbox
-        output = str(
-            check_output(
-                f"Z:\Private\conecommons\scripts\\rom_sync\\download_file.bat {xbox_with_latest_save} {XBOX_PROFILE_ID} {GAME_TITLE_ID} {GAME_SAVE_NAME} {self.console_type}",
-                shell=True,
-            )
+        check_output(
+            f"Z:\Private\conecommons\scripts\\rom_sync\\download_xbox_file.bat {xbox_with_latest_save} {self.profile} {self.config['xbox_games'][self.game]['title_id']}",
+            shell=True,
         )
         print(f"Latest save file downloaded from {xbox_with_latest_save}")
         return xbox_with_latest_save
@@ -130,26 +72,23 @@ class Syncer:
     def upload_latest_save_file(self, xbox_with_latest_save):
         for xbox in self.metadata:
             if xbox != xbox_with_latest_save:
-                output = str(
-                    check_output(
-                        f"Z:\Private\conecommons\scripts\\rom_sync\\upload_file.bat {xbox} {XBOX_PROFILE_ID} {GAME_TITLE_ID} {GAME_SAVE_NAME} {self.console_type}",
-                        shell=True,
-                    )
+                check_output(
+                    f"Z:\Private\conecommons\scripts\\rom_sync\\upload_xbox_file.bat {xbox} {self.profile} {self.config['xbox_games'][self.game]['title_id']}",
+                    shell=True,
                 )
                 print(f"Latest save file uploaded to {xbox}")
 
     def sync_saves(self):
         # connect to each xbox and retrieve metadata for game
         for xbox in self.xboxs:
-            for game in self.game_names:
-                output = str(
-                    check_output(
-                        f"Z:\Private\conecommons\scripts\\rom_sync\\retrieve_metadata.bat {xbox} {self.profile} {self.config["xbox_games"][game]["title_id"]}",
-                        shell=True,
-                    )
+            output = str(
+                check_output(
+                    f"Z:\Private\conecommons\scripts\\rom_sync\\retrieve_xbox_metadata.bat {xbox} {self.profile} {self.config['xbox_games'][self.game]['title_id']}",
+                    shell=True,
                 )
-                last_modified = self.get_last_modified(output, game)
-                self.metadata[xbox][game] = last_modified
+            )
+            last_modified = self.get_last_modified(output)
+            self.metadata[xbox][self.game] = last_modified
         print(self.metadata)
 
         # xbox_with_latest_save = self.get_latest_save_file()
--- test_syncer.py
+++ test_syncer.py
@@ -2,13 +2,12 @@ import os
 from syncer import Syncer
 
 
-METADATA_RESPONSE_SINGLE_SAVE = "b'\r\nZ:\\Private\\dashfox>echo open 192.168.0.233  1>retrieve_metadata.txt \r\n\r\nZ:\\Private\\dashfox>echo user xboxftp xboxftp  1>>retrieve_metadata.txt \r\n\r\nZ:\\Private\\dashfox>echo cd /Hdd1/Content/E00000E4D88A136A/4D53082D/00000001  1>>retrieve_metadata.txt \r\n\r\nZ:\\Private\\dashfox>echo ls  1>>retrieve_metadata.txt \r\n\r\nZ:\\Private\\dashfox>echo quit  1>>retrieve_metadata.txt \r\n\r\nZ:\\Private\\dashfox>ftp -n -s:retrieve_metadata.txt \r\nopen 192.168.0.233 \r\nNot logged in\r\r\nuser xboxftp xboxftp \r\ncd /Hdd1/Content/E00000E4D88A136A/4D53082D/00000001 \r\nls \r\n-rwxrwxrwx   1 root root       1093632 Aug 26 2023 Gears2Checkpoint\r\nquit \r\n\r\nZ:\\Private\\dashfox>del retrieve_metadata.txt \r\n'"
-METADATA_RESPONSE_MULTIPLE_SAVE = "b'\r\nZ:\\Private\\dashfox>echo open 192.168.0.233  1>retrieve_metadata.txt \r\n\r\nZ:\\Private\\dashfox>echo user xboxftp xboxftp  1>>retrieve_metadata.txt \r\n\r\nZ:\\Private\\dashfox>echo cd /Hdd1/Content/E00000E4D88A136A/4D53082D/00000001  1>>retrieve_metadata.txt \r\n\r\nZ:\\Private\\dashfox>echo ls  1>>retrieve_metadata.txt \r\n\r\nZ:\\Private\\dashfox>echo quit  1>>retrieve_metadata.txt \r\n\r\nZ:\\Private\\dashfox>ftp -n -s:retrieve_metadata.txt \r\nopen 192.168.0.233 \r\nNot logged in\r\r\nuser xboxftp xboxftp \r\ncd /Hdd1/Content/E00000E4D88A136A/4D53082D/00000001 \r\nls \r\n-rwxrwxrwx   1 root root       1093632 Aug 26 2023 Gears2Checkpoint\r\nquit \r\n\r\nZ:\\Private\\dashfox>del retrieve_metadata.txt \r\n'"
+METADATA_RESPONSE = "b'\r\nZ:\\Private\\dashfox>echo open 192.168.0.233  1>retrieve_metadata.txt \r\n\r\nZ:\\Private\\dashfox>echo user xboxftp xboxftp  1>>retrieve_metadata.txt \r\n\r\nZ:\\Private\\dashfox>echo cd /Hdd1/Content/E00000E4D88A136A/4D53082D/00000001  1>>retrieve_metadata.txt \r\n\r\nZ:\\Private\\dashfox>echo date -r . +%s  1>>retrieve_metadata.txt \r\n\r\nZ:\\Private\\dashfox>echo quit  1>>retrieve_metadata.txt \r\n\r\nZ:\\Private\\dashfox>ftp -n -s:retrieve_metadata.txt \r\nopen 192.168.0.233 \r\nNot logged in\r\r\nuser xboxftp xboxftp \r\ncd /Hdd1/Content/E00000E4D88A136A/4D53082D/00000001 \r\ndate -r . +%s \r\n1719255989\r\nquit \r\n\r\nZ:\\Private\\dashfox>del retrieve_metadata.txt \r\n'"
 
 
 def test_get_last_modified():
     syncer = Syncer("sjakedude", "gears_of_war_2")
-    res = syncer.get_last_modified(METADATA_RESPONSE, "Gears2Checkpoint")
-    # print(res)
+    res = syncer.get_last_modified(METADATA_RESPONSE)
+    print(res)
 
 test_get_last_modified()
